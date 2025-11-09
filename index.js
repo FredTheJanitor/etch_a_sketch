c3e2a6f9da58dbd4589bf527318701aa91b09583
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-11-09 15:30:39 -0500

    feat: get grid working and make styling good

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // make divs based on selector
 // need input: number of divs
-
+const main = document.querySelector('#main');
 let gridSize = prompt('Enter a number 1-100');
-function createGrid(gridSize) {
-    for (let i = 0; i<(gridSize*gridSize); i++) {
-        const gridSquare = document.createElement('div')
-        gridSquare.id = 'gridSquare'
-        // let squareSize = `(70/${gridSize})-.5`+"vw";
-        // gridSquare.style.flex = `0 0 ((1/${gridSize})*100)%`
-        document.querySelector('#main').appendChild(gridSquare);
+function createGrid(dimensions) {
+    for (i=0;i<dimensions*dimensions;i++) {
+        const newSquare = document.createElement('div');
+        newSquare.classList.add('newSquare');
+        // newSquare.textContent = `number: ${i}`
+        const newSquareSize = (30/dimensions);
+        console.log(newSquareSize);
+        newSquare.style.width = `${newSquareSize}vw`
+        newSquare.style.height = `${newSquareSize}vw`
+        main.appendChild(newSquare);  
     }
 }
 createGrid(gridSize);
